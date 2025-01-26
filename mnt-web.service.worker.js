565c0fd3e1ca51685cf9fc0fe8cd3f825cbfd0db
Kreatiwi <79002555+Kreatiwi@users.noreply.github.com>
2025-01-26 14:14:20 +0100

    Updated Web (Fixes)

--- mnt-web.service.worker.js
+++ mnt-web.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737893365|92148591';
+const CACHE_VERSION = '1737897088|712513351';
 /** @type {string} */
 const CACHE_PREFIX = 'MNT-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
