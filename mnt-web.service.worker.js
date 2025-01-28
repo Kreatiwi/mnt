0dc52c0cfc060cd9c2f5badf095eef7ad4d03c12
Kreatiwi <79002555+Kreatiwi@users.noreply.github.com>
2025-01-28 14:59:17 +0100

    Updated Game and downloads

--- mnt-web.service.worker.js
+++ mnt-web.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737897088|712513351';
+const CACHE_VERSION = '1738072596|778649259';
 /** @type {string} */
 const CACHE_PREFIX = 'MNT-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
